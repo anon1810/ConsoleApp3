48caf2dbe24b6f66e70be9110233da650477cac1
Anon Hongglin <Anon.h@bangchak.co.th>
2024-02-13 16:00:57 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x = "test";
+            bool y = false;
+
+            var result = x + y;
+            string xx = "";
+        }
+    }
+}
